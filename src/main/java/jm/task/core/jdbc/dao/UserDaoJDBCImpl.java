cd7d38ceae02f4b7b1c2ee3a88799598e003edb1
redgroul <redgroulman@gmail.com>
2023-12-27 16:40:54 +0300

    исправления типа данных столбца возраста при создании таблицы

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
@@ -21,7 +22,7 @@ public class UserDaoJDBCImpl implements UserDao {
         }
 
         try {
-            statement.executeUpdate("CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT, name varchar(64), lastName varchar(64), age bit(20))");
+            statement.executeUpdate("CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT, name varchar(64), lastName varchar(64), age tinyint)");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -40,7 +41,19 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            String sql = "INSERT INTO users (`name`, `lastname`, `age`) VALUES ((?), (?), (?))";
 
+        //    Statement statement = new Util().getConnection().createStatement();
+            PreparedStatement statement = new Util().getConnection().prepareStatement(sql);
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3,age);
+            statement.executeUpdate();
+
+        } catch (Exception e) {
+
+        }
     }
 
     public void removeUserById(long id) {
